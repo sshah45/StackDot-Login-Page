bc582676d3e2f9379081f716cc492ee429ce3b84
sujay shah <sujayshah@sujays-MacBook-Air.local>
2025-07-13 19:39:01 +0530

    Auth token related fix.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,9 +8,15 @@ export default function Dashboard() {
             <div className="container">
                 <div className="row">
                     <h1 className="fw-bold text-center py-5">Welcome to the Dashboard</h1>
-                    <div className="text-center">
-                        <Link to="/" className="p-3 bg-primary text-white under border-1 rounded-1 text-decoration-none">Logout</Link>
+                    <div className="d-flex justify-content-center gap-5">
+                        <div className="text-center" onClick={() => localStorage.clear()}>
+                            <Link to="/" className="p-3 bg-primary text-white under border-1 rounded-1 text-decoration-none">Logout</Link>
+                        </div>
+                        <div className="text-center">
+                            <Link to="/" className="p-3 bg-primary text-white under border-1 rounded-1 text-decoration-none">Go black</Link>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </>
--- src/LogIn.js
+++ src/LogIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 
@@ -8,10 +8,25 @@ function LogInPage() {
         password: "",
     });
     const [error, setError] = useState("");
-
-
     const navigate = useNavigate();
 
+    useEffect(() => {
+    const handleFormData = () => {
+        if(localStorage.getItem("token")){
+            setFormdata({
+                email: dummyData.email,
+                password: dummyData.password,
+            })
+        }else {
+            setFormdata({
+                email: "",
+                password: "",
+            })
+        }
+    }
+    handleFormData();
+    },[]);
+    
     const dummyData = {
         email: "abc@gmail.com",
         password: "Abcd@1234",
